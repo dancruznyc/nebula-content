8124cbf5fb3093b711586cfdbcd15a64a3aab00b
dancruznyc <dancruznyc@gmail.com>
2021-11-24 07:48:50 -0500

    hw assignment

---
+++ 11-dom-manipulation/assignments/box-dom-hw/script.js
@@ -0,0 +1,23 @@
+//using a loop create 10 colored boxes
+// The colors should be random
+//have a background color
+//
+body = document.body;
+container = document.createElement("div");
+container.className = "container";
+body.appendChild(container);
+
+const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+for (let i = 1; i <= 10; i++) {
+  let div = document.createElement("div");
+  let hex = "";
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * hexValues.length);
+    hex += hexValues[index];
+  }
+  div.className = "div-size";
+  div.innerText = `Div ${i}`;
+  div.style.backgroundColor = `#${hex}`;
+  container.appendChild(div);
+}
